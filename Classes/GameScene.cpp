e0041d4f763a083c52aa2995d3bb14586c406828
hieunq <hieunq@ows.vn>
2017-01-22 23:34:41 +0700

    add keyboard event

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -29,7 +29,31 @@ bool GameScene::init(){
 	Tank* player = new Tank();
     player = Tank::create(100,100,0);
     this->addChild(player);
-    
+
+    auto eventListener = EventListenerKeyboard::create();
+
+    eventListener->onKeyPressed = [player](EventKeyboard::KeyCode keyCode, Event* event){
+        switch(keyCode){
+            case EventKeyboard::KeyCode::KEY_LEFT_ARROW:
+            case EventKeyboard::KeyCode::KEY_A:
+                player->rotateLeft();
+                break;
+            case EventKeyboard::KeyCode::KEY_RIGHT_ARROW:
+            case EventKeyboard::KeyCode::KEY_D:
+                player->rotateRight();
+                break;
+            case EventKeyboard::KeyCode::KEY_UP_ARROW:
+            case EventKeyboard::KeyCode::KEY_W:
+                player->moveForward();
+                break;
+            case EventKeyboard::KeyCode::KEY_DOWN_ARROW:
+            case EventKeyboard::KeyCode::KEY_S:
+                player->moveBack();
+                break;
+        }
+    };
+
+    this->_eventDispatcher->addEventListenerWithSceneGraphPriority(eventListener,player);
     
     return true;
 }
